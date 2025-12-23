3ae5f33404c9e734e4230db21f69c6f61a72d007
Shahil Basnet <shahilbasnet434@gmail.com>
2025-12-24 00:20:40 +0545

    Fix: commented out validateDog method since it was only necessary for one place; Add: validation logic in addDog method; Add: delete logic in deleteDog

--- src/Controller/DogController.java
+++ src/Controller/DogController.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
+
 import Model.Dog;
 import java.util.LinkedList;
 
@@ -11,67 +12,83 @@ import java.util.LinkedList;
  * @author Shahil
  */
 public class DogController {
+
     /**
-     * Adds "Dog" object to the linkedList 
-     * 
+     * Adds "Dog" object to the linkedList
+     *
      * @param dogList linkedList of DogModel
      * @param dogObject object of dog with its data to add to the linkedList
      * @return true if dog is added, else returns false
      * @throws
      */
-    public boolean addDog(LinkedList<Dog> dogList, Dog dogObject){
-        
+    public boolean addDog(LinkedList<Dog> dogList, Dog dogObject) {
+        for (Dog dog : dogList) {
+            if (dogObject.getId() == dog.getId()) {
+                return false;
+            }
+        }
+        dogList.add(dogObject);
         return true;
     }
-    
+
     /**
-     * Deletes existing "Dog" object from the linkedList 
-     * 
+     * Deletes existing "Dog" object from the linkedList
+     *
      * @param dogList linkedList of DogModel
+     * @param dogId id of the dog to delete
      * @return true if successfully deleted dog, else returns false
      * @throws
      */
-    public boolean deleteDog(LinkedList<Dog> dogList){
+    public boolean deleteDog(LinkedList<Dog> dogList, int dogId) {
+        for (int i = 0; i < dogList.size(); i++) {
+            if (dogList.get(i).getId() == dogId) {
+                dogList.remove(i);
+                return true;
+            }
+        }
         return false;
     }
+
     /**
      * Updates details of existing "Dog" object in linkedList
-     * 
+     *
      * @param dogList arrayList of DogModel
      * @return true if successfully updated dog's details, else returns false
      * @throws
      */
-    public boolean updateDog(LinkedList<Dog> dogList){
-        
+    public boolean updateDog(LinkedList<Dog> dogList) {
+
         return true;
     }
+
     /**
-     * Validates details "Dog" object
-     *  Checks if the dogId exists when adding, deleting, updating or viewing a dog
-     * 
+     * Validates details "Dog" object Checks if the dogId exists when adding,
+     * deleting, updating or viewing a dog
+     *
      * @param dogId
      * @param mode
      * @param dogList linkedList of DogModel
      * @return true if validation is successful, false if it fails
      * @throws
      */
-    public boolean validateDog(int dogId, LinkedList<Dog> dogList, String mode){
-        if(mode.equalsIgnoreCase("add")){
-            for(Dog dog: dogList){
-                if(dogId == dog.getId()){
-                    return false;
-                }
-            }
-            return true;
-        }
-        //if mode: update, delete
-        else{
-            for(Dog dog: dogList){
-                if(dogId == dog.getId()){
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
+    //    public boolean validateDog(int dogId, LinkedList<Dog> dogList, String mode) {
+    //        if (mode.equalsIgnoreCase("add")) {
+    //            for (Dog dog : dogList) {
+    //                if (dogId == dog.getId()) {
+    //                    return false;
+    //                }
+    //            }
+    //            return true;
+    //        } //if mode: update, delete
+    //        else {
+    //            for (Dog dog : dogList) {
+    //                if (dogId == dog.getId()) {
+    //                    return true;
+    //                }
+    //            }
+    //            return false;
+    //        }
+    //    }
+    
+    //WILL USE LATER IF NEEDED
 }
